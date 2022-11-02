e6d041650fe72c4961c004160f8e295bd9c731f6
Student <jvaladez92000@gmail.com>
2022-11-01 19:40:47 -0700

    file for fs functions

---
+++ mfs.c
@@ -0,0 +1,28 @@
+/**************************************************************
+* Class:  CSC-415-03 Fall 2021
+* Names:  Richard Aguilar
+*         Melisa Sever
+*         Ryan Scott
+*         Jonathan Valadez
+*
+* Student IDs: 977075554
+*              921662115
+*              921814228
+*              922274961
+*
+* GitHub Name: raguilar0917
+* Group Name: The Beerman Fan Club
+* Project: Basic File System
+*
+* File: mfs.c
+*
+* Description: To be updated... 
+*
+**************************************************************/
+
+int main(){
+
+
+
+return 0;
+}
