9270bcdcd3ddb052a9174608e7f56e724bbe0c8d
Student <jvaladez92000@gmail.com>
2022-11-01 20:07:41 -0700

    mfs.c template update

--- mfs.c
+++ mfs.c
@@ -19,10 +19,58 @@
 * Description: To be updated... 
 *
 **************************************************************/
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <dirent.h>
 
-int main(){
+#include "mfs.h"
+#include "b_io.h"
+
+
+// Key directory functions
+int fs_mkdir(const char *pathname, mode_t mode){
+
+};
+
+int fs_rmdir(const char *pathname){
+
+};
+
+// Directory iteration functions
+fdDir * fs_opendir(const char *pathname){
+
+};
+
+struct fs_diriteminfo *fs_readdir(fdDir *dirp){
+
+};
+
+int fs_closedir(fdDir *dirp){
+
+};
+
+// Misc directory functions
+char * fs_getcwd(char *pathname, size_t size){
+
+};
+
+int fs_setcwd(char *pathname){       //linux chdir
+
+}; 
+ 
+int fs_isFile(char * filename){     //return 1 if file, 0 otherwise
+
+};
+	
+int fs_isDir(char * pathname){      //return 1 if directory, 0 otherwise
+
+};	
+	
+int fs_delete(char* filename){      //removes a file
+
+};	
 
 
 
-return 0;
-}
+
